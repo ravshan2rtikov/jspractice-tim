2542fb8f872d33c3a097ff7233ee4acd1c8c9c70
ravshan2rtikov <ravshan2rtikov@gmail.com>
2023-10-12 22:29:50 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+console.log('Welcome to the Computer Hardware Quiz!');
+
+const answer1 = prompt('What is the brain of the computer?: ');
+const correct_answer1 = 'CPU';
