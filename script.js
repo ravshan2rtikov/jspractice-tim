97e934296dfab6d0e293103d01490922501c9caf
ravshan2rtikov <ravshan2rtikov@gmail.com>
2023-10-12 22:40:34 +0400

    Added first if else statements

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ console.log('Welcome to the Computer Hardware Quiz!');
 
 const answer1 = prompt('What is the brain of the computer?: ');
 const correct_answer1 = 'CPU';
+
+if (answer1.toUpperCase() === correct_answer1) {
+  console.log('You got it Correct!');
+} else {
+  console.log('You got it Wrong!');
+}
