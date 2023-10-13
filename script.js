046ef82a6391d595d3b0c9698b83e6a1d73a9e65
ravshan2rtikov <ravshan2rtikov@gmail.com>
2023-10-13 11:01:46 +0400

    Added results variables

--- script.js
+++ script.js
@@ -2,20 +2,39 @@ const prompt = require('prompt-sync')();
 
 console.log('Welcome to the Computer Hardware Quiz!');
 
-const answer1 = prompt('What is the brain of the computer?: ');
-const correct_answer1 = 'CPU';
+let correctAnswers = 0;
+const totalQuestions = 3;
 
-if (answer1.toUpperCase() === correct_answer1) {
+
+const answer1 = prompt('What is the brain of the computer?: ');
+const correctAnswer1 = 'CPU';
+
+if (answer1.toUpperCase() === correctAnswer1) {
   console.log('You got it Correct!');
+  correctAnswers++;
 } else {
   console.log('You got it Wrong!');
 }
 
 const answer2 = prompt('What is better a 3090ti pr 4090ti?: ');
-const correct_answer2 = '3090ti';
+const correctAnswer2 = '3090ti';
 
-if (answer2.toLowerCase() === correct_answer2) {
+if (answer2.toLowerCase() === correctAnswer2) {
   console.log('You got it Correct!');
+  correctAnswers++;
 } else {
   console.log('You got it Wrong!');
 }
+
+const answer3 = prompt('What is a recommend RAM in 2023?: ');
+const correctAnswer3 = '16GB';
+
+if (answer3.toUpperCase() === correctAnswer3) {
+  console.log('You got it Correct!');
+  correctAnswers++;
+} else {
+  console.log('You got it Wrong!');
+}
+
+console.log('You got ' + correctAnswers + ' out of 3 questions correct!');
+console.log('You scored ' + (correctAnswers / totalQuestions) * 100 + ' percent!');
